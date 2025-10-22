fb357ae65cd37c07a1d01a4fefbcb89350f83fa4
1durch0 <github10@gibmir.net>
2025-10-22 14:07:13 +0200

    test

--- calc.py
+++ calc.py
@@ -4,18 +4,18 @@ print("This is a calculator!")
 print("You can add, subtract, multiply, divide numbers and find the modulus!")
 print("Use add for addition\nsub for substraction\nmultiply for multiplication\ndivide for division\nmod for modulus")
 def add(x, y):
-    return x + y
+    return int(x) + int(y)
 def sub(x, y):
-    return x - y
+    return int(x) - int(y)
 def mul(x, y):
-    return x * y
+    return int(x) * int(y)
 def div(x, y):
-    return x / y
+    return int(x) / int(y)
 def mod(x, y):
-    return x % y
+    return int(x) % int(y)
 print("Write your operator here:")
-x = input()
-match x:
+z = input()
+match z:
     case 'add':
         print(add(input(), input()))
     case 'sub':
