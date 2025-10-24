6f2994d66a72238d12a9f98b72d2dae31e08a6a0
1durch0 <github10@gibmir.net>
2025-10-24 13:35:36 +0200

    test

--- Week2/calc.py
+++ Week2/calc.py
@@ -1,6 +1,6 @@
 print("This is a calculator!")
 print("You can add, subtract, multiply, divide numbers and find the modulus!")
-print("Use add for addition\nsub for substraction\nmultiply for multiplication\ndivide for division\nmod for modulus")
+print("Use add for addition, sub for substraction, multiply for multiplication, divide for division, mod for modulus")
 def add(x, y):
     return int(x) + int(y)
 def sub(x, y):
@@ -12,22 +12,22 @@ def div(x, y):
 def mod(x, y):
     return int(x) % int(y)
 
-
-print("Write your operator here:")
-z = input()
-def calc(z):
-    match z:
+def calc():
+    operator = str(input("Write your operator here: "))
+    match operator:
         case 'add':
-            print(add(input(), input()))
+            return add(input("Put your first number here: "), input("Put your second number here: "))
         case 'sub':
-            print(sub(input(), input()))
+            return sub(input("Put your first number here: "), input("Put your second number here: "))
         case 'multiply':
-            print(mul(input(), input()))
+            return mul(input("Put your first number here: "), input("Put your second number here: "))
         case 'multiply':
-            print(mul(input(), input()))
+            return mul(input("Put your first number here: "), input("Put your second number here: "))
         case 'divide':
-            print(div(input(), input()))
+            return div(input("Put your first number here: "), input("Put your second number here: "))
         case 'mod':
-            print(mod(input(), input()))
+            return mod(input("Put your first number here: "), input("Put your second number here: "))
         case _:
-            print("Error: wrong input")
+            return("Error: wrong input")
+
+print(calc())
