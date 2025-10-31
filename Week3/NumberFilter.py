90f056a86ed92dd99b9d864ccd4970d176d20963
1durch0 <github10@gibmir.net>
2025-10-31 09:59:05 +0100

    test

--- Week3/NumberFilter.py
+++ Week3/NumberFilter.py
@@ -1,15 +1,21 @@
 MyNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
-for number in MyNumbers:
-    oddNumbers = []
-    if number % 2 == 0:
-        continue
-    oddNumbers.append(MyNumbers)
-    print(MyNumbers)
 
-for number in MyNumbers:
+def oddNumbersCount(array):
+    oddNumbers = []
+    for number in array:
+        if number % 2 == 0:
+            continue
+        oddNumbers.append(number)
+    return print(oddNumbers)
+
+def evenNumbersCount(array):
     evenNumbers = []
-    if number % 2 != 0:
-        continue
-    evenNumbers.append(MyNumbers)
-    print(MyNumbers)
+    for number in array:
+        if number % 2 != 0:
+            continue
+        evenNumbers.append(number)
+    return print(evenNumbers)
+
+oddNumbersCount(MyNumbers)
+evenNumbersCount(MyNumbers)
