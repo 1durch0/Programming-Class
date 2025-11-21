9599561b273a0a412f650b9150dc9161d64c4ee0
1durch0 <github10@gibmir.net>
2025-11-21 09:33:16 +0100

    Woche 5 2.0

--- Week5/password2.0.py
+++ Week5/password2.0.py
@@ -1,1 +1,47 @@
-blabka
+import string
+from passwordsData import passwords
+
+def check_min_length(password, min_length=8):
+    return len(password) >= min_length
+
+def check_max_length(password, max_length=16):
+    return len(password) <= max_length
+
+def check_uppercase(password):
+    return any(char.isupper() for char in password)
+
+def check_lowercase(password):
+    return any(char.islower() for char in password)
+
+def check_digit(password):
+    return any(char.isdigit() for char in password)
+
+def check_special_char(password):
+    return any(char in string.punctuation for char in password)
+
+def validate_password(password):
+    is_valid = True
+    password_validation = {}
+    if not check_min_length(password):
+        is_valid = False
+    if not check_max_length(password):
+        is_valid = False
+    if not check_uppercase(password):
+        is_valid = False
+    if not check_lowercase(password):
+        is_valid = False
+    if not check_digit(password):
+        is_valid = False
+    if not check_special_char(password):
+        is_valid = False
+    
+    if is_valid == True:
+        password_validation[password] = True
+    else:
+        password_validation[password] = False
+
+    print(f"{password}: {password_validation[password]}")
+
+for key in passwords.keys():
+    validate_password(key)
+
--- Week5/passwordsData.py
+++ Week5/passwordsData.py
@@ -4,4 +4,5 @@ passwords = {
     "carol": "F1refly$83",
     "dave": "G@laxyRun77",
     "eve": "Sh1ftLock&22",
+    "frank": "N3bula*55",
 }
