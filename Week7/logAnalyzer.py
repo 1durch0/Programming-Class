743df67d5c66eb2366d2f0494db7e4e93012c29e
1durch0 <github10@gibmir.net>
2026-01-09 14:19:40 +0100

    Refactor logging configuration in logAnalyzer.py for improved readability

--- Week7/logAnalyzer.py
+++ Week7/logAnalyzer.py
@@ -1,10 +1,9 @@
 import logging
 
 logging.basicConfig(
-level=logging.INFO,
-format='%(asctime)s - %(levelname)s - %(message)s',
-handlers=[
-logging.FileHandler('analysis_audit.log'),
-logging.StreamHandler()
-]
-)
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+    logging.FileHandler('analysis_audit.log'),
+    logging.StreamHandler()
+])
